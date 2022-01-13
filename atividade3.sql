1737d2d35ad5bb4a7fd3c9688c14338d08ef4596
gstv-web <gustavo>
2022-01-13 13:45:15 -0300

    Correção de dados

--- atividade3.sql
+++ atividade3.sql
@@ -1,11 +1,13 @@
 CREATE DATABASE atividade3;
 
+USE atividade3;
+
 CREATE TABLE tb_alunos(
 id INT(2) AUTO_INCREMENT,
 Nome VARCHAR(100),
 Curso VARCHAR(50),
 Unidade VARCHAR(50),
-Nota DECIMAL,
+Nota DECIMAL(2,1),
 PRIMARY KEY(id)
 );
 
@@ -13,8 +15,8 @@ INSERT INTO tb_alunos(Nome, Curso, Unidade, Nota) VALUES ("Aline Rodrigues Castr
 ("Rodrigo Araújo", "Matemática", "Mooca", 7.7),
 ("Guilherme Jorge Silva Carvalho", "Inglês", "Bom Retiro", 6.5),
 ("Aline Silveira Santos", "Geografia", "Vila Mariana", 9.8),
-("Kleber da Silva Pires", "Português", "Mooca", 7),
-("Mariana Silva Santos", "Matemática", "Mooca", 6),
+("Kleber da Silva Pires", "Português", "Mooca", 7.3),
+("Mariana Silva Santos", "Matemática", "Mooca", 6.6),
 ("Juliana Mara de Oliveira", "Biologia", "Vila Mariana", 4),
 ("Fabiana Pereira", "Inglês", "Bom Retiro", 9.5);
 
